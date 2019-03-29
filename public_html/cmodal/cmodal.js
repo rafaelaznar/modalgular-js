cd074250319b3cfc1e02412e388992f1b8d1f9fe
Rafael Aznar <rafaaznar@gmail.com>
2019-03-29 19:52:14 +0100

    add onpage modal

--- public_html/cmodal/cmodal.js
+++ public_html/cmodal/cmodal.js
@@ -5,22 +5,28 @@ moduloDirectivas.component('caddModalVar', {
         arrayDeVariables: '='
     },
     controllerAs: 'c',
-    controller: addModalVarController   
+    controller: addModalVarController
 });
 
 function addModalVarController($http) {
-        var self = this;
-        self.newVariableAndValue = {
-            name: '',
-            value: ''
-        };
-        self.save = function () {
-            var tempVariableAndValue = {
-                name: self.newVariableAndValue.name,
-                value: self.newVariableAndValue.value
-            }
-            self.arrayDeVariables.push(tempVariableAndValue);
-        };
-    }
-    
-    
+    var self = this;
+    self.newVariableAndValue = {
+        name: '',
+        value: ''
+    };
+    self.save = function () {
+        var tempVariableAndValue = {
+            name: self.newVariableAndValue.name,
+            value: self.newVariableAndValue.value
+        }
+        self.arrayDeVariables.push(tempVariableAndValue);
+    };
+//    self.$onInit = function () {
+//        //ajax 
+//        var respuestaAJAX={name:"ooooooo",value:"oooooooooo"};
+//        
+//        self.arrayDeVariables.push(respuestaAJAX);
+//    };
+}
+
+
--- public_html/index.js
+++ public_html/index.js
@@ -1,4 +1,35 @@
 app.controller('indexControl', function ($scope) {
     $scope.variables = [];
     $scope.variables = [{name: "variable 1 pre-cargada", value: 1}, {name: "variable 2 pre-cargada", value: 2}]
+    
+    
+    
+    
+    $scope.newVariableAndValue = {
+        name: '',
+        value: ''
+    };
+    $scope.save = function () {
+        var tempVariableAndValue = {
+            name: $scope.newVariableAndValue.name,
+            value: $scope.newVariableAndValue.value
+        }
+        $scope.variables.push(tempVariableAndValue);
+    };
+    $scope.initialize = function () {
+        // posible llamada ajax 
+        var respuestaAJAX={name:"repuesta AJAX al cargar la modal",value:"oooooooooo"};       
+        $scope.variables.push(respuestaAJAX);
+        
+    };
+//    self.$onInit = function () {
+//        //ajax 
+//        var respuestaAJAX={name:"ooooooo",value:"oooooooooo"};
+//        
+//        self.arrayDeVariables.push(respuestaAJAX);
+//    };
+    
+    
+    
+    
 });
